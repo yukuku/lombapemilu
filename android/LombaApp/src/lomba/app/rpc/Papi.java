e39ea7a8935adb76411a88f5ed6452b0c170851f
Andreas Wong <andreaswong770@gmail.com>
2014-03-08 21:27:47 +0800

    Splash skrin

--- android/LombaApp/src/lomba/app/rpc/Papi.java
+++ android/LombaApp/src/lomba/app/rpc/Papi.java
@@ -15,7 +15,7 @@ public class Papi {
 	public static final String TAG = Papi.class.getSimpleName();
 	private static final String APIKEY = "201042adb488aef2eb0efe21bdd3ca7f";
 
-	static String BASE = "http://192.168.43.238/lomba_git/server/api.php";
+	static String BASE = "http://192.168.13.144/lomba_git/server/api.php";
 
 	static AsyncHttpClient client = new AsyncHttpClient();
 
