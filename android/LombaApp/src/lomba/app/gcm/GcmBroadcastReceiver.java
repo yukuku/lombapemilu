c9b5c4af95c357439461526117f98edd0c51572c
Yuku on yuku5 <yukuku@gmail.com>
2014-03-31 20:08:53 +0800

    implement client gcm

---
+++ android/LombaApp/src/lomba/app/gcm/GcmBroadcastReceiver.java
@@ -0,0 +1,27 @@
+package lomba.app.gcm;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+public class GcmBroadcastReceiver extends BroadcastReceiver {
+	private static final String TAG = GcmBroadcastReceiver.class.getSimpleName();
+
+	public void onReceive(Context context, Intent intent) {
+		Log.d(TAG, "Got intent");
+		Log.d(TAG, "  action: " + intent.getAction());
+		Log.d(TAG, "  data uri: " + intent.getData());
+		Log.d(TAG, "  component: " + intent.getComponent());
+		Log.d(TAG, "  flags: 0x" + Integer.toHexString(intent.getFlags()));
+		Log.d(TAG, "  mime: " + intent.getType());
+		Bundle extras = intent.getExtras();
+		Log.d(TAG, "  extras: " + (extras == null? "null": extras.size()));
+		if (extras != null) {
+			for (String key: extras.keySet()) {
+				Log.d(TAG, "    " + key + " = " + extras.get(key));
+			}
+		}
+	}
+}
