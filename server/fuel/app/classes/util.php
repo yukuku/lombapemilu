9a4e705af0418bcffadb52ff741c8bb901422296
Yuku on yuku5 <yukuku@gmail.com>
2014-03-31 19:51:28 +0800

    SUsah diungkapkan

--- server/fuel/app/classes/util.php
+++ server/fuel/app/classes/util.php
@@ -12,8 +12,8 @@ class Util {
 	public static function notifyUsers($dapilId, $caleg, $inputs) {
 		//Prepare message payload
 
-		Log::debug('content: ' . var_dump($inputs['content'], 1));
-		Log::debug('title: ' . var_dump($inputs['title'], 1));
+		Log::debug('content: ' . var_export($inputs['content'], 1));
+		Log::debug('title: ' . var_export($inputs['title'], 1));
 
 		$data = array(
 			'kind' => 'new_caleg_rating',
